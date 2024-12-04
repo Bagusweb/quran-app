8d3143b3877d21a2e927489022ac7387af33838d
bagus erlangga <159453277+Bagusweb@users.noreply.github.com>
2024-12-04 19:31:46 +0700

    Update ReadQuranHeader.tsx
    b

--- src/components/Header/ReadQuranHeader.tsx
+++ src/components/Header/ReadQuranHeader.tsx
@@ -5,7 +5,7 @@ import Search from '@components/Search';
 type Props = {};
 
 const ReadQuranHeader = (props: Props) => {
-  return <Header search={<Search />}>Baca Quran</Header>;
+  return <Header search={<Search />}>Baca Quran Bagus</Header>;
 };
 
 export default ReadQuranHeader;
