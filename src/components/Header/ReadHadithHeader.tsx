6ee51799db1a9ae28333f72275a253ea6716381e
bagus erlangga <159453277+Bagusweb@users.noreply.github.com>
2024-12-04 20:19:23 +0700

    Update ReadHadithHeader.tsx
    hadis

--- src/components/Header/ReadHadithHeader.tsx
+++ src/components/Header/ReadHadithHeader.tsx
@@ -4,7 +4,7 @@ import Header from '.';
 type Props = {};
 
 const ReadHadithHeader = (props: Props) => {
-  return <Header className="mb-5">Baca Hadits</Header>;
+  return <Header className="mb-5">Baca Hadits Bagus</Header>;
 };
 
 export default ReadHadithHeader;
